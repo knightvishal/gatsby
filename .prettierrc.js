defbf5c64eb3ee526c250afcb326f4fa745fcaec
VISHAL BHARTI <42848830+knightvishal@users.noreply.github.com>
2019-10-04 22:58:27 +0530

    little corrections
    little corrections

--- .prettierrc.js
+++ .prettierrc.js
@@ -3,12 +3,12 @@ module.exports = {
   semi: false,
   singleQuote: false,
   tabWidth: 2,
-  trailingComma: "es5",
+  trailingComma: " es5 ",
   overrides: [
     {
       // This file uses semicolons. It's needed here because `documentation`
       // package (used to parse jsdoc and provide content for API reference pages)
-      // behaviour is inconsistent when not using semicolons after
+      // behaviour is inconsistent when not using semicolons after.
       // object declarations.
       files: ["**/api-node-helpers-docs.js"],
       options: {
